28ba66a9ccedead0fe118411a5ea7ce73cd4f573
Shreyas <shreyasvgaiki2000@gmail.com>
2023-03-29 16:59:27 +0530

    Grid View

--- app/src/main/java/com/example/newsfeed/MainActivity.java
+++ app/src/main/java/com/example/newsfeed/MainActivity.java
@@ -1,33 +1,104 @@
 package com.example.newsfeed;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    CarouselView carouselView;
-    int[] sampleImages = {R.drawable.news1, R.drawable.news2, R.drawable.news3, R.drawable.news4 };
+    private RecyclerView recyclerView;
+    private ArrayList<RecyclerData> recyclerDataArrayList;
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        recyclerView=findViewById(R.id.idCourseRV);
 
-        carouselView = findViewById(R.id.carouselView);
-        carouselView.setPageCount(sampleImages.length);
-        carouselView.setImageListener(imageListener);
+        // created new array list..
+        recyclerDataArrayList=new ArrayList<>();
 
+        // added data to array list
+        recyclerDataArrayList.add(new RecyclerData("Business",R.drawable.download));
+        recyclerDataArrayList.add(new RecyclerData("Entertainment",R.drawable.download));
+        recyclerDataArrayList.add(new RecyclerData("General",R.drawable.download));
+        recyclerDataArrayList.add(new RecyclerData("Health",R.drawable.download2));
+        recyclerDataArrayList.add(new RecyclerData("Science",R.drawable.download2));
+        recyclerDataArrayList.add(new RecyclerData("Sports",R.drawable.download2));
+        recyclerDataArrayList.add(new RecyclerData("Technology",R.drawable.download2));
+
+        // added data from arraylist to adapter class.
+        RecyclerViewAdapter adapter=new RecyclerViewAdapter(recyclerDataArrayList,this);
+
+        // setting grid layout manager to implement grid view.
+        // in this method '2' represents number of columns to be displayed in grid view.
+        GridLayoutManager layoutManager=new GridLayoutManager(this,3);
+
+        // at last set adapter to recycler view.
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
-
-    ImageListener imageListener = new ImageListener() {
-        @Override
-        public void setImageForPosition(int position, ImageView imageView) {
-            imageView.setImageResource(sampleImages[position]);
-        }
-    };
-
 }
+
+
+
+
+
+
+
+
+
+
+
+//import android.os.Bundle;
+//import android.widget.Toolbar;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    CarouselView carouselView;
+//    int[] sampleImages = {R.drawable.news1, R.drawable.news2, R.drawable.news3, R.drawable.news4 };
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        carouselView = findViewById(R.id.carouselView);
+//        carouselView.setPageCount(sampleImages.length);
+//        carouselView.setImageListener(imageListener);
+//
+//    }
+//
+//    ImageListener imageListener = new ImageListener() {
+//        @Override
+//        public void setImageForPosition(int position, ImageView imageView) {
+//            imageView.setImageResource(sampleImages[position]);
+//        }
+//    };
+//
+//}
