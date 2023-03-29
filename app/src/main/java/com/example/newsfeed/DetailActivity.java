937da5997d5ed86c731a093153def129d1353232
5um1t <asumitkadam@gmail.com>
2023-03-29 11:11:21 +0530

    added detail page

---
+++ app/src/main/java/com/example/newsfeed/DetailActivity.java
@@ -0,0 +1,40 @@
+package com.example.newsfeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class DetailActivity extends AppCompatActivity {
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_page);
+
+        String title,desc,content,imageUrl,url;
+
+        title=getIntent().getStringExtra("title");
+        desc=getIntent().getStringExtra("desc");
+        content=getIntent().getStringExtra("content");
+        imageUrl=getIntent().getStringExtra("image");
+        url=getIntent().getStringExtra("url");
+
+        //42:58 time GFG video
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
