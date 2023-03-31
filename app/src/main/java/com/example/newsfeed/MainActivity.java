152fba752ded14e3ae6228aeb2005bbbb8043b05
Shreyas <shreyasvgaiki2000@gmail.com>
2023-03-31 10:29:02 +0530

    removed unwanted comments

--- app/src/main/java/com/example/newsfeed/MainActivity.java
+++ app/src/main/java/com/example/newsfeed/MainActivity.java
@@ -78,34 +78,3 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-//import android.os.Bundle;
-//import android.widget.Toolbar;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    CarouselView carouselView;
-//    int[] sampleImages = {R.drawable.news1, R.drawable.news2, R.drawable.news3, R.drawable.news4 };
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        carouselView = findViewById(R.id.carouselView);
-//        carouselView.setPageCount(sampleImages.length);
-//        carouselView.setImageListener(imageListener);
-//
-//    }
-//
-//    ImageListener imageListener = new ImageListener() {
-//        @Override
-//        public void setImageForPosition(int position, ImageView imageView) {
-//            imageView.setImageResource(sampleImages[position]);
-//        }
-//    };
-//
-//}
